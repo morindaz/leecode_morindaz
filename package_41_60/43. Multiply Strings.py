3a4397d819e5bd5d48e4262b123cc30ce23316cb
Your Name <you@example.com>
2019-03-10 00:30:41 +0800

    43 继续需要在做

---
+++ package_41_60/43. Multiply Strings.py
@@ -0,0 +1,56 @@
+"""
+Given two non-negative integers num1 and num2 represented as strings,
+
+return the product of num1 and num2, also represented as a string.
+
+Example 1:
+Input: num1 = "2", num2 = "3"
+Output: "6"
+Example 2:
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+Note:
+
+The length of both num1 and num2 is < 110.
+Both num1 and num2 contain only digits 0-9.
+Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+"""
+
+
+def change_value(self, str_value):
+
+
+class Solution(object):
+
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        change_dict = {
+            "0": 0,
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9
+        }
+        res = 0
+        for num_2 in num2:
+            tmp = 0
+            for num_1 in num1:
+                tmp = tmp * 10 + int(num_1) * int(num_2)
+            res = res * 10 + tmp
+        return str(res)
+
+
+if __name__ == '__main__':
+    solution = Solution().multiply("123","456")
+    print(solution)
