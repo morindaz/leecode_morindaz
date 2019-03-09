ad95bc3100a67092d02c1b5f911ca7d250a60f65
Your Name <you@example.com>
2019-03-09 22:25:07 +0800

    2 38

--- package_1_20/2. Add Two Numbers.py
+++ package_1_20/2. Add Two Numbers.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 '''
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two non-empty linked lists representing two non-negative integers. T
+
+he digits are stored in reverse order and each of their nodes contain a single digit.
+
+Add the two numbers and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -31,19 +35,73 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-a1 = ListNode(2)
-a2 = ListNode(4)
-a3 = ListNode(3)
-a1.next = a2
-a2.next = a3
-b1 = ListNode(5)
-b2 = ListNode(6)
-b3 = ListNode(4)
-b1.next = b2
-b2.next = b3
-l1 = a1
-l2 = b1
-while((l1!=None and l2!=None)):
-    print(l1.val, l2.val)
-    l1 = l1.next
-    l2 = l2.next
+        flag = 0
+
+        head_val = l1.val + l2.val
+        if head_val >= 10:
+            head_val -= 10
+            head = ListNode(head_val)
+            flag = 1
+        else:
+            head = ListNode(head_val)
+        res = head
+        l1 = l1.next
+        l2 = l2.next
+
+        while(l1 != None and l2 != None):
+            tmp_val = l1.val + l2.val+ flag
+            flag = 0
+            if tmp_val >= 10:
+                tmp_list = ListNode(tmp_val - 10 )
+                flag = 1
+            else:
+                tmp_list = ListNode(tmp_val)
+            head.next = tmp_list
+            head = head.next
+            l1 = l1.next
+            l2 = l2.next
+        if l1 == None and l2!= None:
+            self.append_rest(flag, l2, head)
+        elif l2 == None and l1 != None:
+            self.append_rest(flag, l1, head)
+        elif flag!= 0:
+            tmp_list = ListNode(flag)
+            head.next = tmp_list
+        return res
+
+    def append_rest(self, flag, rest_list, head):
+        while(rest_list != None):
+            tmp_val = rest_list.val + flag
+            flag = 0
+            if tmp_val >= 10:
+                tmp_list = ListNode(tmp_val - 10)
+                flag = 1
+            else:
+                tmp_list = ListNode(tmp_val)
+            head.next = tmp_list
+            head = head.next
+            rest_list = rest_list.next
+        if flag != 0:
+            tmp_list = ListNode(flag)
+            head.next = tmp_list
+            # l2 = l2.next
+
+if __name__ == '__main__':
+    a1 = ListNode(5)
+    # a2 = ListNode(8)
+    # a3 = ListNode(3)
+    # a1.next = a2
+    # a2.next = a3
+    b1 = ListNode(5)
+    # b2 = ListNode(9)
+    # b3 = ListNode(4)
+    # b1.next = b2
+    # b2.next = b3
+    l1 = a1
+    l2 = b1
+
+    solution = Solution().addTwoNumbers(l1, l2)
+    while(solution!=None):
+        print(solution.val)
+        solution = solution.next
+        # l2 = l2.next
---
+++ package_21_40/38. Count and Say.py
@@ -0,0 +1,67 @@
+"""
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+
+
+Example 1:
+
+Input: 1
+Output: "1"
+Example 2:
+
+Input: 4
+Output: "1211"
+"""
+
+
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        result = ''
+        for i in range(n):
+            result = self.get_next_sequence(result)
+        return result
+
+    def get_next_sequence(self, previous_sequence):
+        left = 0
+        next_sequence = ''
+        if len(previous_sequence) == 0:
+            return '1'
+        for right in range(len(previous_sequence)):
+            if right == len(previous_sequence) - 1 or previous_sequence[right] != previous_sequence[right+1]:
+                next_sequence = self.say(previous_sequence[left: right+1], next_sequence)
+                left = right + 1
+        return next_sequence
+
+
+    def say(self, sequence, result):
+        tmp = str(len(sequence)) + sequence[0]
+        result += tmp
+        return result
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    solution = Solution().countAndSay(4)
+    print(solution)
+
