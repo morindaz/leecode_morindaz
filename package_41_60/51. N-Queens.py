04e11dd9a705c7273dc44e1526e50bf84d7f37c2
Your Name <you@example.com>
2019-07-16 23:07:39 +0800

    增加最小公共祖先

--- package_41_60/51. N-Queens.py
+++ package_41_60/51. N-Queens.py
@@ -1,7 +1,19 @@
 """
 The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
 """
-
+class Solution2(object):
+    def solveNQuees(self, n):
+        def DFS(queens, xy_dif, xy_sum):
+            p = len(queens)
+            if p==n:
+                result.append(queens)
+                return None
+            for q in range(n):
+                if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
+                    DFS(queens + [q], xy_dif+[p-q], xy_sum+[p+q])
+        result = []
+        DFS([],[],[])
+        return [["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
 
 class Solution(object):
     def solveNQueens(self, n):
