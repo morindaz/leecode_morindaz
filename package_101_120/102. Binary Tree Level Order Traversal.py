2976548f2bceb04fb6c7cfcd7166c184125fca0f
Your Name <you@example.com>
2019-07-18 23:48:13 +0800

    BFS还有DFS

---
+++ package_101_120/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+import collections
+class Solution(object):
+    """
+    广度优先的方法，用queue
+    """
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        result = []
+        queue = collections.deque()
+        queue.append(root)
+        visited = set(root)
+        while queue:
+            level_size = len(queue)
+            current_level = []
+            for _ in range(level_size):
+                node = queue.popleft()
+                current_level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            result.append(current_level)
+        return result
+
+
+class Solution2(object):
+    def levelOrder(self, root):
+        if not  root:
+            return []
+        self.result = []
+        self._dfs(root, 0)
+        return self.result
+
+    def _dfs(self, node, level):
+        if not node:
+            return
+        if len(self.result) < level + 1: #说明当前行没有任何结果
+            self.result.append([])
+
+        self.result[level].append(node.val)
+        self._dfs(node.left, level +1)
+        self._dfs(node.right, level +1)
---
+++ package_101_120/104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,40 @@
+"""
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+if __name__ == '__main__':
+    solution = Solution()
